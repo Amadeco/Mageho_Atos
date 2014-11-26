8f09bacd3af1f58eecd92486d4a11e269927b447
mageho <ilan.parmentier@mageho.com>
2014-11-26 17:35:54 +0100

    Fix undefined index in admin
    Fix an issue with undefined index for info.phtml in stantard & several
    payment method.

--- app/code/local/Mageho/Atos/Model/System/Config/Source/Cms/Block.php
+++ app/code/local/Mageho/Atos/Model/System/Config/Source/Cms/Block.php
@@ -1,4 +1,22 @@
 <?php
+/*
+ * Mageho
+ * Ilan PARMENTIER
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0) that is available
+ * through the world-wide-web at this URL: http://www.opensource.org/licenses/OSL-3.0
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to contact@mageho.com so we can send you a copy immediately.
+ *
+ * @category     Mageho
+ * @package     Mageho_Atos
+ * @author       Mageho, Ilan PARMENTIER <contact@mageho.com>
+ * @copyright   Copyright (c) 2014  Mageho (http://www.mageho.com)
+ * @license      http://www.opensource.org/licenses/OSL-3.0  Open Software License (OSL 3.0)
+ */
+ 
 class Mageho_Atos_Model_System_Config_Source_Cms_Block
 {
     protected $_options;
@@ -16,4 +34,4 @@ class Mageho_Atos_Model_System_Config_Source_Cms_Block
         }
         return $this->_options;
     }
-} 
+}
